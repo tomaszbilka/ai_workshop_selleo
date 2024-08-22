5208be14270b1584bc2b390a0af16d9550ddb5de
tomaszbilka <bila4@op.pl>
2024-08-22 06:46:20 +0200

    refactor ai service

--- apps/api/src/aichat/aichat.service.ts
+++ apps/api/src/aichat/aichat.service.ts
@@ -4,26 +4,35 @@ import { technicians } from "./constants";
 
 @Injectable()
 export class AichatService {
-  getAiResponse = async (query: string) => {
-    const client = new OpenAI({
+  private client: OpenAI;
+
+  constructor() {
+    this.client = new OpenAI({
       apiKey: process.env.OPENAI_KEY,
     });
+  }
 
-    const prompt = `Problem: ${query}
-Lista specjalistów: ${JSON.stringify(technicians)}
+  private createPropmt = (query: string): string => `Problem: ${query}
+    Lista specjalistów: ${JSON.stringify(technicians)}
 
-Twoim zadaniem jest zidentyfikowanie specjalisty, którego umiejętności są najbardziej odpowiednie do rozwiązania podanego problemu.
+    Twoim zadaniem jest zidentyfikowanie specjalisty, którego umiejętności są najbardziej odpowiednie do rozwiązania podanego problemu.
 
-Jeśli taka osoba istnieje, zwróć wynik w formacie: ID=<tutaj wstaw id>; Imię i Nazwisko; Opis, jak i dlaczego ta osoba może rozwiązać wskazany problem.
-Jeśli żadna osoba nie spełnia kryteriów, koniecznie odpowiedz: "Nie znalazłem odpowiedniego specjalisty."
-Uwaga: Jeśli nie ma odpowiedniego specjalisty, musisz wyraźnie to zaznaczyć w odpowiedzi.`;
+    Jeśli taka osoba istnieje, zwróć wynik w formacie: ID=<tutaj wstaw id>; Imię i Nazwisko; Opis, jak i dlaczego ta osoba może rozwiązać wskazany problem.
+    Jeśli żadna osoba nie spełnia kryteriów, koniecznie odpowiedz: "Nie znalazłem odpowiedniego specjalisty."
+    Uwaga: Jeśli nie ma odpowiedniego specjalisty, musisz wyraźnie to zaznaczyć w odpowiedzi.`;
 
-    const chatCompletion = await client.chat.completions.create({
+  private fetchAiResponse = async (prompt: string): Promise<string[]> => {
+    const chatCompletion = await this.client.chat.completions.create({
       messages: [{ role: "user", content: prompt }],
       model: "gpt-3.5-turbo",
     });
-    const answer = chatCompletion.choices[0].message.content?.split(";");
+    return chatCompletion.choices[0].message.content?.split(";") || [];
+  };
 
-    return JSON.stringify(answer);
+  getAiResponse = async (query: string) => {
+    const prompt = this.createPropmt(query);
+    const response = await this.fetchAiResponse(prompt);
+
+    return JSON.stringify(response);
   };
 }
